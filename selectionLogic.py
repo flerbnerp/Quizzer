105fb5dbe829208229a707dcd6a4a4588e6382e2
Aaron Raycove <aacra0820@gmail.com>
2023-08-27 12:12:08 -0600

    fixed algorithm, it was adding duplicate questions to the question batch, test shows it selects the correct amount of questions and no duplicate questions in the list

--- selectionLogic.py
+++ selectionLogic.py
@@ -7,7 +7,7 @@ QA_DIR = "qaJsonFiles"
 CONFIG_DIR = "config"
 CONFIG_FILE = "qaCategoriesConfig.json"
 
-def select_questions_from_file(filepath, num_questions_to_pick=1):
+def select_questions_from_file(filepath, num_questions_to_pick=1, last_selected=[]):
     with open(filepath, 'r') as file:
         qa_data = json.load(file)
 
@@ -35,12 +35,17 @@ def select_questions_from_file(filepath, num_questions_to_pick=1):
     tied_questions = [qa for score, qa in scored_qa if score == threshold_score]
     
     if len(tied_questions) > 1:
+        # Filter out questions from the last batch
+        tied_questions = [qa for qa in tied_questions if qa not in last_selected]
         random.shuffle(tied_questions)
-        num_tied_to_pick = num_questions_to_pick - scored_qa.index((threshold_score, tied_questions[0])) 
-        # Use a list comprehension to maintain (score, qa) format for tied_questions
+        num_tied_to_pick = num_questions_to_pick - scored_qa.index((threshold_score, tied_questions[0]))
         selected_qas = scored_qa[:num_questions_to_pick - num_tied_to_pick] + [(threshold_score, qa) for qa in tied_questions[:num_tied_to_pick]]
     else:
         selected_qas = scored_qa[:num_questions_to_pick]
+
+    # Update last_selected for the next call
+    last_selected.clear()
+    last_selected.extend([qa for _, qa in selected_qas])
 
     return [(qa['question'], qa['answer']) for _, qa in selected_qas]
 
@@ -62,6 +67,13 @@ def get_weighted_question():
         selected_qas = select_questions_from_file(filepath, num_questions_to_pick)
         for q, a in selected_qas:
             main_list.append((q, a, filepath))
+# Check length of main_list
     print (len(main_list))
+# Check for duplicates in main_list
+    unique_questions = set([q for q, _, _ in main_list])
+    if len(unique_questions) != len(main_list):
+        print(f"There are {len(main_list) - len(unique_questions)} duplicate questions.")
+    else:
+        print("No duplicate questions found.")
     return main_list  # Return the full list of question-answer pairs
 
