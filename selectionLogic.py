1ed6a2f7f565cbd0295c1e6e61c9b2b2b3aceff7
Aaron Raycove <aacra0820@gmail.com>
2023-08-28 17:31:34 -0600

    Noticed that questions were consistently being asked if user consistently had trouble answering them, while this is desired, it can be a frustrating user experience. Instead the algorithm has been updated to make the random factor more apparent and to decrease the influence that wrong answers have on the selection process, increased the affect of how often a question has been shown to the user has on the selection process

--- selectionLogic.py
+++ selectionLogic.py
@@ -20,9 +20,9 @@ def select_questions_from_file(filepath, num_questions_to_pick=1, last_selected=
         total_attempts = qa.get("total_attempts", 0)
         correct_attempts = qa.get("correct_attempts", 0)
         ratio = 0 if total_attempts == 0 else correct_attempts / total_attempts
-        random_factor = random.random()
+        random_factor = random.randint(0,100)
         try:
-            score = 0.495 * ratio + 0.495 * (correct_attempts / max_correct_attempts) + 0.01 * random_factor
+            score = 35 * ratio + 50 * (correct_attempts / max_correct_attempts) + 0.10 * random_factor
         except ZeroDivisionError:
             score = 0
         scored_qa.append((score, qa))
