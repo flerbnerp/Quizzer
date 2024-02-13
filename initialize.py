29611c1edbee0d14461b6edf746227536e447bf0
Aaron Raycove <aacra0820@gmail.com>
2024-02-13 12:38:41 -0700

    overhaul and rebuild

--- initialize.py
+++ initialize.py
@@ -1,98 +1,31 @@
-import json
 import os
-import subprocess
-
-CONFIG_FILE = "config.json"
-
-def create_config():
-    """Initialize a new configuration file."""
-    # Check if the config file already exists
-    if os.path.exists(CONFIG_FILE):
-        print(f"{CONFIG_FILE} already exists.")
-        return
-    
-    # Create an empty config file
-    with open(CONFIG_FILE, 'w') as file:
-        json.dump({}, file)
-    print(f"{CONFIG_FILE} has been created.")
-
-def read_config():
-    """Read the configuration file and return its contents."""
-    if os.path.exists(CONFIG_FILE):
-        with open(CONFIG_FILE, 'r') as file:
-            config = json.load(file)
-        return config
-    else:
-        print(f"{CONFIG_FILE} does not exist.")
-        return None
-
-def save_to_config(key, value):
-    """Utility function to save data to config file."""
-    if os.path.exists(CONFIG_FILE):
-        with open(CONFIG_FILE, 'r') as file:
-            config = json.load(file)
-    else:
-        config = {}
-    config[key] = value
-    with open(CONFIG_FILE, 'w') as file:
-        json.dump(config, file)
-
-def get_from_config(key):
-    """Utility function to retrieve data from config file."""
-    if os.path.exists(CONFIG_FILE):
-        with open(CONFIG_FILE, 'r') as file:
-            config = json.load(file)
-            return config.get(key)
-    return None
-
-def find_education_directory():
-    """Search for the 'Education/' directory within the /home directory and return its path."""
-    
-    command = ['find', '/home', '-type', 'd', '-iname', 'Education']
-    completed_process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True)
-    
-    result = completed_process.stdout.strip().split('\n')
-    
-    if result and result[0]:
-        # Assuming there might be multiple Education directories, take the first found one
-        education_dir = result[0]
-        
-        # Save this directory to the config for future use
-        save_to_config("education_directory", education_dir)
-        
-        return education_dir
-    else:
-        print("Education directory not found.")
-        return None
-
-def initialize_education_directory():
-    # Create a config file
-    create_config()
-    # Read the created/available config
-    config_content = read_config()
-    if config_content is not None:
-        print("")
-    
-    # Check if the directory is already in the config file
-    existing_directory = get_from_config('education_directory')
-    if existing_directory:
-        print(f"Education directory found in config: {existing_directory}")
-        
-        # Validate if the existing_directory is still valid
-        if os.path.exists(existing_directory) and os.path.isdir(existing_directory):
-            print(f"Education directory is valid: {existing_directory}")
-            return
-        else:
-            print(f"Education directory in config is no longer valid. Searching for a new directory...")
-
-    # If not in the config or the existing directory is invalid, search for the Education directory
-    education_dir = find_education_directory()
-    if education_dir:
-        print(f"New Education directory found: {education_dir}")
-        save_to_config('education_directory', education_dir)
-    else:
-        print("Education directory not found.")
-
-
-if __name__ == "__main__":
-    initialize_education_directory()
+import yaml
+# use a dictionary
+# Concept/question, subject, related
+def scan_directory():
+    questions = []
+    concepts = []
+    for root,dirs, files in os.walk("/home/karibar/Documents/Education"):
+        for file in files:
+            if file.endswith(".md"):
+                with open(os.path.join(root,file), "r", encoding="utf-8") as f:
+                    content = f.read()  
+                start_delimiter, end_delimiter = "---", "---\n"
+                if start_delimiter and end_delimiter:
+                    start_index = content.find(start_delimiter) + len(start_delimiter)
+                    end_index = content.find(end_delimiter, start_index)
+                    if start_index > -1 and end_index > -1:
+                        yaml_properties = content[start_index:end_index].strip()
+                        try:
+                            note_dict = yaml.safe_load(yaml_properties)
+                            filename, extension = os.path.splitext(os.path.basename(file))
+                            full_filename = f"{filename}.{extension}"
+                            note_dict["file_name"] = full_filename
+                            if note_dict["type"] == "Concept":
+                                concepts.append(note_dict)
+                            if note_dict["type"] == "question":
+                                questions.append(note_dict)
+                        except:
+                            pass
+    return concepts, questions 
+scan_directory()
--- main.py
+++ main.py
@@ -1,33 +1,106 @@
-from initialize import initialize_education_directory
-from initializeFilePath import construct_filepaths_directory
-from initializeQaCategories import initialize_qa_categories
-from gui import guiMain
-import signal
-
-def handle_interrupt(signum, frame):
-    print("Exiting...")
-    exit(0)
-
-def main():
-    # Initialize directories and config files
-    initialize_education_directory()
-    construct_filepaths_directory()
-    initialize_qa_categories()
-    # Set up interrupt handling
-    signal.signal(signal.SIGINT, handle_interrupt)
-    guiMain.start_gui()
+from initialize import scan_directory
+import subprocess
+import os
+import random
+def populate_question_list(concepts, questions):
+    question_list = [] # empty the question list, prevents need to pass question_list into the function
+    number_of_concepts = 18
+    number_of_questions = 25
+    while len(question_list) < number_of_concepts: # populates int(concepts) into the question list 
+        rand = random.randint(0, len(concepts))
+        question_list.append(concepts[rand])
+    while len(question_list) < number_of_questions: # populates the difference betwene num_concepts and num_questions if 18 and 20, this populates 2 questions into the list
+        rand = random.randint(0, len(questions))
+        question_list.append(questions[rand])
+    random.shuffle(question_list)
+    return question_list   
+def begin_quiz(concepts, questions):
+    question_list = []
+    while True:
+        if len(question_list) > 0:
+            user_input = input("\n\nEnter any key to continue: ")
+            os.system("clear")
+            if user_input == "exit":
+                os.system("clear")
+                break
+            # Quiz Interface
+            print(f"File name: {question_list[0]['file_name']}")
+            try:
+                print(f"Type: {question_list[0]['sub-type']}")
+            except:
+                pass
+            print(f"Subject Matter: {question_list[0]['subject']}")
+            # For concept notes:
+            if f"{question_list[0]['type']}" == "Concept":
+                print(f"\nPlease explain the following concept and what concepts might related to it: {question_list[0]['file_name']}")
+                input(f"Enter any key to reveal related concepts: ")
+                print(f"Related Concepts: {question_list[0]['related']}")
+            # For question notes:
+            if f"{question_list[0]['type']}" == "question":
+                print(f"\nAnswer the following question:\n\n {question_list[0]['question_text']}")
+                input("\nEnter any key to reveal the answer: ")
+                print(f"\nRelated concepts: {question_list[0]['related']}")
+                try:
+                    print(f"{question_list[0]['answer_text']}")
+                except:
+                    print(f"no defined answer, check concept file")
+            # Remove the item from the list.
+            question_list.pop(0)
+        elif len(question_list) <= 0: #Once the list is empty, go back and grab a new set of questions:
+            print("Getting new set of questions. . .")
+            question_list = populate_question_list(concepts, questions)
+        else:
+            pass
 
 if __name__ == "__main__":
-    main()
-    
-#Github instructions
-# Run this first if you have made changes to the software on another system. Best to
-# run this before you start working on the software on your system.
-# git fetch origin
-# git pull origin main
+    # If it takes an excessively long time to scan_directory, then we can simply add in the scan_directory as a menu option and update scan to write to file, for now it's only a few seconds to scan, If
+    # takes longer than a minute, then likely it would be beneficial to optimize.
+    # Currently its about 2000 notes and only a few seconds to initialize. Given this it would require 10's of thousands of notes to become a problem
+    concepts, questions = scan_directory()
+    error = False
+    while True:
+        print("Welcome to Quizzer")
+        print(f"There are {len(concepts)} concept notes and {len(questions)} question notes")
+        print("You will be tested on a mixture of concepts and questions")
+        print("Please select a menu option to begin")
+        print("1: begin quiz")
+        print("2: exit program")
+        if error == True:
+            print("Enter a valid menu option:")
+            error = False
+        user_input = input("User: ")
+        if user_input == "1":
+            os.system("clear")
+            begin_quiz(concepts, questions)
+        elif user_input == "2":
+            break
+        else:
+            error = True
+        os.system("clear")  
+        
+# Planned changes:
+## add in historical_person notes to quizzer, this list will quiz on birth and death dates
+### Who the person was and why they were important:
 
-# Run these commands to push your changes to the repository
-# git status
-# git add .
-# git commit -m "message"
-# git push origin main
+## add in config options
+### First option to tell quizzer, how many of each type of question to present in each quiz
+
+## Scoring methodology
+### get filename.md
+### add scoring metrics
+### add above to json file for permanent storage
+### Scan will check whether filename.md in concepts, questions, people, 
+### already exists in the scoreboard.json. If not, add in new json object to represent the file:
+### every object will contain the yaml properties, subject, related, question_text, answer_text, 
+#### Scores should never be overwritten, but json objects should update based when 
+#### files in obsidian changes
+
+## Update quiz function to take advantage of scoring metrics
+### this will require quizzer to pull questions from the .json file instead of from a prescanned list.
+### Updates quiz function will have an algorithm to determine how to populate the quiz based on current scoring metrics:
+
+## add in config option: questions by subject
+### Only effects the question type
+
+# Change Log
+## 
